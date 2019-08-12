372ed5e9b4a18858d0807d17bc74d83b6bc9a6b5
Clifford Sutter <css1990x@gmail.com>
2019-08-11 21:22:28 -0400

    Create pascalsTriangle.js

---
+++ pascalsTriangle.js
@@ -0,0 +1,37 @@
+
+const pascalsTriangle = (n) => {
+    // iterative solution
+    if (n < 1) {
+        return 'Please enter a number greater than or equal to one';
+    }
+    let first = [1];
+    let second = [1, 1];
+    let final = []; 
+    final.push(first);
+    if (n === 1) {
+        return final;
+    }
+    final.push(second);
+    if (n === 2) {
+        return final
+    }
+    let a =  2;
+    let start = second;
+    while (a < n){
+        let len = start.length; 
+        let current = []; 
+        for (let x = 1; x < start.length; x++){
+            current.push(start[x] + start[x - 1]); 
+        }
+        current.unshift(1);
+        current.push(1);
+        final.push(current);
+        start = current;
+        a++;
+    }
+    return final; 
+}
+
+let pascal = pascalsTriangle(3);
+console.log({ pascal });
+console.log(pascalsTriangle(process.argv[2]));
